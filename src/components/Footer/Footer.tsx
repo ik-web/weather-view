d8e14470ff783e290a7614c2deaf9d98b018016a
Ihor Kuchin <vedunigor@gmail.com>
2022-10-06 22:14:36 +0300

    Added Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h4 className={styles.footer__title}>
+        WeatherView app
+      </h4>
+
+      <p>
+        Performer is
+        <a
+          href='https://bit.ly/3CbRw25'
+          target={`${'_'}blank`}
+          className={styles.footer__link}
+        >IK-web</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
